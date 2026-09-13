47e0c32c1128486f0df2e05b31871e1536afd1bb
Selvakumar Boominathan <selvans2k@gmail.com>
2026-09-13 17:23:13 +0530

    implementation of {{this}} usage in all bronze layer data.

--- aws_dbt_snowflake_project/models/bronze/bronze_listings.sql
+++ aws_dbt_snowflake_project/models/bronze/bronze_listings.sql
@@ -4,5 +4,4 @@ SELECT * FROM {{ source('staging', 'listings') }}
 {% if is_incremental() %}
   {% set incremental_column = 'CREATED_AT' %}
   WHERE {{ incremental_column }} > (SELECT COALESCE( MAX({{ incremental_column }}), '1900-01-01') FROM {{ this }} )
-
 {% endif %}
